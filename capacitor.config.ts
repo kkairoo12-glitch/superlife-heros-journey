37bb4bb7ddf6e3d4086212a4ef945369294d944c
kkairoo12-glitch <kkairoo12@gmail.com>
2026-04-23 19:50:58 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.superlife.hero',
+  appName: "Superlife Hero's Journey",
+  webDir: 'dist'
+};
+
+export default config;
